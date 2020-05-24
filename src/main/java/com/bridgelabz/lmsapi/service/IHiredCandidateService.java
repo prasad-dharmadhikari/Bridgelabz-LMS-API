147446168c14f187431c2b410af814181cf4dc7c
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-05-25 00:35:10 +0530

    ADDED : Methods for new mappings

--- src/main/java/com/bridgelabz/lmsapi/service/IHiredCandidateService.java
+++ src/main/java/com/bridgelabz/lmsapi/service/IHiredCandidateService.java
@@ -5,11 +5,15 @@ import com.bridgelabz.lmsapi.dto.Response;
 import com.bridgelabz.lmsapi.model.HiredCandidate;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.mail.MessagingException;
 import java.util.List;
 
 public interface IHiredCandidateService {
     Response saveDataInBatchToDatabase(MultipartFile file);
     List getHiredCandidateList();
-    void save(HiredCandidateDTO hiredCandidateDTO);
+    void save(HiredCandidateDTO hiredCandidateDTO) throws MessagingException;
     HiredCandidate getCandidateProfile(long id);
+    Response updateCandidateStatus(String response, String email);
+    void sendMail(HiredCandidate hiredCandidate) throws MessagingException;
+    Response joinCandidate() throws MessagingException;
 }
