6cbe8a1dc67a4645b2d9769167dce78d346bd4de
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-05-24 14:01:04 +0530

    ADDED : Test and dependencies file

---
+++ src/test/java/com/bridgelabz/lmsapi/LmsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.lmsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
