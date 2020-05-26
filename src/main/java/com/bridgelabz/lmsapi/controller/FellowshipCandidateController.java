b895f79a7a41164a2fdaa65ac84fa06790040fb4
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-05-26 16:23:42 +0530

    ADDED : API for joining the candidate and save the data in fellowship table

--- src/main/java/com/bridgelabz/lmsapi/controller/FellowshipCandidateController.java
+++ src/main/java/com/bridgelabz/lmsapi/controller/FellowshipCandidateController.java
@@ -1,4 +1,21 @@
 package com.bridgelabz.lmsapi.controller;
 
+import com.bridgelabz.lmsapi.dto.FellowshipCandidateDTO;
+import com.bridgelabz.lmsapi.dto.Response;
+import com.bridgelabz.lmsapi.service.IFellowshipCandidateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/fellowshipcandidate")
 public class FellowshipCandidateController {
+
+    @Autowired
+    private IFellowshipCandidateService fellowshipCandidateService;
+
+    @PostMapping("/jointhecandidate")
+    public ResponseEntity<Response> joinCandidate(@RequestBody FellowshipCandidateDTO fellowshipCandidateDTO) {
+        return ResponseEntity.ok(fellowshipCandidateService.joinTheCandidateToFellowshipProgram(fellowshipCandidateDTO));
+    }
 }
