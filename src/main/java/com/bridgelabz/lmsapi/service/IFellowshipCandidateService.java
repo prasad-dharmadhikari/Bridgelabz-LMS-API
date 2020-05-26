00568410ed63ab421c6d580cac7dcaa93090ad35
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-05-26 16:30:17 +0530

    ADDED : Interface for fellowship candidate service

--- src/main/java/com/bridgelabz/lmsapi/service/IFellowshipCandidateService.java
+++ src/main/java/com/bridgelabz/lmsapi/service/IFellowshipCandidateService.java
@@ -1,4 +1,8 @@
 package com.bridgelabz.lmsapi.service;
 
+import com.bridgelabz.lmsapi.dto.FellowshipCandidateDTO;
+import com.bridgelabz.lmsapi.dto.Response;
+
 public interface IFellowshipCandidateService {
+    Response joinTheCandidateToFellowshipProgram(FellowshipCandidateDTO fellowshipCandidateDTO);
 }
