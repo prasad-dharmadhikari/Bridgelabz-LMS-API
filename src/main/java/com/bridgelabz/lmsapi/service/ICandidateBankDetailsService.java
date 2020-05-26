96938cd5bda9860e73bda7f6de0acc04a16c2dac
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-05-27 02:36:40 +0530

    ADDED : Interface for candidate bank details service

--- src/main/java/com/bridgelabz/lmsapi/service/ICandidateBankDetailsService.java
+++ src/main/java/com/bridgelabz/lmsapi/service/ICandidateBankDetailsService.java
@@ -1,4 +1,8 @@
 package com.bridgelabz.lmsapi.service;
 
+import com.bridgelabz.lmsapi.dto.CandidateBankDetailsDTO;
+import com.bridgelabz.lmsapi.dto.Response;
+
 public interface ICandidateBankDetailsService {
+    Response updateCandidateBankInfo(CandidateBankDetailsDTO candidateBankDetailsDTO);
 }
