e3bba67fab6f3e9aa25fb2f5460cc5f6b717c7c3
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-05-27 02:23:28 +0530

    ADDED : Mappings to update bank info

--- src/main/java/com/bridgelabz/lmsapi/controller/CandidateBankDetailsController.java
+++ src/main/java/com/bridgelabz/lmsapi/controller/CandidateBankDetailsController.java
@@ -1,4 +1,24 @@
 package com.bridgelabz.lmsapi.controller;
 
+import com.bridgelabz.lmsapi.dto.CandidateBankDetailsDTO;
+import com.bridgelabz.lmsapi.dto.Response;
+import com.bridgelabz.lmsapi.service.ICandidateBankDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/candidatebank")
 public class CandidateBankDetailsController {
+
+    @Autowired
+    private ICandidateBankDetailsService candidateBankDetailsService;
+
+    @PostMapping("/bankinfo")
+    public ResponseEntity<Response> updateCandidateBankInfo(@RequestBody CandidateBankDetailsDTO candidateBankDetailsDTO) {
+        return ResponseEntity.ok(candidateBankDetailsService.updateCandidateBankInfo(candidateBankDetailsDTO));
+    }
 }
