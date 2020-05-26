6fbc9353edffc3fecf8194caa57215fb31d240b5
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-05-26 16:25:32 +0530

    ADDED : DTO for Fellowship candidate

--- src/main/java/com/bridgelabz/lmsapi/dto/FellowshipCandidateDTO.java
+++ src/main/java/com/bridgelabz/lmsapi/dto/FellowshipCandidateDTO.java
@@ -1,4 +1,43 @@
 package com.bridgelabz.lmsapi.dto;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Getter(AccessLevel.PUBLIC)
+@Setter(AccessLevel.PUBLIC)
 public class FellowshipCandidateDTO {
+    private long id;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String email;
+    private String hiredCity;
+    private String degree;
+    private Date hiredDate;
+    private long mobileNumber;
+    private long permanentPincode;
+    private String hiredLab;
+    private String attitude;
+    private String communicationRemark;
+    private String knowledgeRemark;
+    private String aggregateRemark;
+    private String status;
+    private Date birthDate;
+    private String isBirthDateVerified;
+    private String parentName;
+    private String parentOccupation;
+    private long parentMobileNumber;
+    private int parentAnnualSalary;
+    private String localAddress;
+    private String permanentAddress;
+    private String photoPath;
+    private LocalDate joiningDate;
+    private String documentStatus;
+    private String remark;
+    private Date creatorStamp;
+    private String creatorUser;
 }
