5531a5a4b23f65ee636ad41209754a341a11d7aa
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-05-26 16:27:22 +0530

    ADDED : Repository for fellowship candidate

--- src/main/java/com/bridgelabz/lmsapi/repository/FellowshipCandidateRepository.java
+++ src/main/java/com/bridgelabz/lmsapi/repository/FellowshipCandidateRepository.java
@@ -1,4 +1,10 @@
 package com.bridgelabz.lmsapi.repository;
 
-public class FellowshipCandidateRepository {
+import com.bridgelabz.lmsapi.model.FellowshipCandidate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FellowshipCandidateRepository extends JpaRepository<FellowshipCandidate, Long> {
+
 }
