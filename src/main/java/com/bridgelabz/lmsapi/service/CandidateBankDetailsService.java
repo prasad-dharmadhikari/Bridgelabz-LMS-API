207a901f9dd09176a7c96be2c188d93a33a7f8c9
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-05-27 02:33:44 +0530

    ADDED : Service for candidate bank details

--- src/main/java/com/bridgelabz/lmsapi/service/CandidateBankDetailsService.java
+++ src/main/java/com/bridgelabz/lmsapi/service/CandidateBankDetailsService.java
@@ -1,4 +1,33 @@
 package com.bridgelabz.lmsapi.service;
 
-public class CandidateBankDetailsService {
+import com.bridgelabz.lmsapi.dto.CandidateBankDetailsDTO;
+import com.bridgelabz.lmsapi.dto.Response;
+import com.bridgelabz.lmsapi.model.CandidateBankDetails;
+import com.bridgelabz.lmsapi.repository.CandidateBankDetailsRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
+
+@Service
+public class CandidateBankDetailsService implements ICandidateBankDetailsService {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private CandidateBankDetailsRepository candidateBankDetailsRepository;
+
+    @Override
+    public Response updateCandidateBankInfo(CandidateBankDetailsDTO candidateBankDetailsDTO) {
+        candidateBankDetailsDTO.setIsAccountNumberVerified("Yes");
+        candidateBankDetailsDTO.setIsAdhaarNumberVerified("Yes");
+        candidateBankDetailsDTO.setIsIfscCodeVerified("Yes");
+        candidateBankDetailsDTO.setIsPanNumberVerified("Yes");
+        candidateBankDetailsDTO.setCreatorStamp(LocalDateTime.now());
+        candidateBankDetailsDTO.setCreatorUser(candidateBankDetailsDTO.getAccountName());
+        CandidateBankDetails candidateBankDetails = modelMapper.map(candidateBankDetailsDTO, CandidateBankDetails.class);
+        candidateBankDetailsRepository.save(candidateBankDetails);
+        return new Response(200, "Bank Details Updated Successfully");
+    }
 }
