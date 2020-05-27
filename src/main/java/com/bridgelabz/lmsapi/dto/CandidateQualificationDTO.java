bbbe8079a418011d196940e2f524089e00283760
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-05-27 23:11:03 +0530

    ADDED : DTO for candidate qualification

--- src/main/java/com/bridgelabz/lmsapi/dto/CandidateQualificationDTO.java
+++ src/main/java/com/bridgelabz/lmsapi/dto/CandidateQualificationDTO.java
@@ -1,4 +1,33 @@
 package com.bridgelabz.lmsapi.dto;
 
+import com.bridgelabz.lmsapi.model.FellowshipCandidate;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter(AccessLevel.PUBLIC)
+@Setter(AccessLevel.PUBLIC)
 public class CandidateQualificationDTO {
+    public long Id;
+    public FellowshipCandidate fellowshipCandidate;
+    public String diplomaDegreeName;
+    public String isDegreeNameVerified;
+    public String employeeDiscipline;
+    public String isEmployeeDisciplineVerified;
+    public String passingYear;
+    public String isPassingYearVerified;
+    public Double aggregatePercentage;
+    public String isAggregatePercentageVerified;
+    public Double finalYearPercentage;
+    public String isFinalYearPercentageVerified;
+    public String trainingInstitute;
+    public String isTrainingInstituteVerified;
+    public String trainingDurationMonth;
+    public String isTrainingDurationMonthVerified;
+    public String otherTraining;
+    public String isOtherTrainingVerified;
+    public LocalDateTime creatorStamp;
+    public String creatorUser;
 }
