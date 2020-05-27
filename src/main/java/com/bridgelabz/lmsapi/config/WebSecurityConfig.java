e9660dfb5a162328d55f96097d32b02a02e8d64e
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-05-27 23:06:54 +0530

    ADDED : Permission for new endpoints

--- src/main/java/com/bridgelabz/lmsapi/config/WebSecurityConfig.java
+++ src/main/java/com/bridgelabz/lmsapi/config/WebSecurityConfig.java
@@ -71,7 +71,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                         "/swagger-resources/**"
                         , "/swagger-ui.html", "/v2/api-docs"
                         , "/webjars/**","/fellowshipcandidate/jointhecandidate",
-                "/fellowshipcandidate/getcandidatecount","/candidatebank/bankinfo").permitAll().
+                "/fellowshipcandidate/getcandidatecount","/candidatebank/bankinfo",
+                        "/candidatequalification/educationalinfo").permitAll().
                 // all other requests need to be authenticated
                         anyRequest().authenticated().and().
                 // make sure we use stateless session; session won't be used to
