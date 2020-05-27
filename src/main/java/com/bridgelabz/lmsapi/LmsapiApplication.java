810c364376680c1d4e4c814cca7734f4aed8ed53
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-05-27 23:04:47 +0530

    ADDED : Permissons for new endpoints

--- src/main/java/com/bridgelabz/lmsapi/LmsapiApplication.java
+++ src/main/java/com/bridgelabz/lmsapi/LmsapiApplication.java
@@ -2,12 +2,12 @@ package com.bridgelabz.lmsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-public class LmsapiApplication {
 
+public class LmsapiApplication {
     public static void main(String[] args) {
         SpringApplication.run(LmsapiApplication.class, args);
     }
-
 }
