521365ef8e22c20b85ca738ee4b089c09571a84f
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-05-27 23:14:48 +0530

    ADDED : Services for candidate qualification details

--- src/main/java/com/bridgelabz/lmsapi/service/CandidateQualificationService.java
+++ src/main/java/com/bridgelabz/lmsapi/service/CandidateQualificationService.java
@@ -1,4 +1,37 @@
 package com.bridgelabz.lmsapi.service;
 
-public class CandidateQualificationService {
+import com.bridgelabz.lmsapi.dto.CandidateQualificationDTO;
+import com.bridgelabz.lmsapi.dto.Response;
+import com.bridgelabz.lmsapi.model.CandidateQualification;
+import com.bridgelabz.lmsapi.repository.CandidateQualificationRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
+
+@Service
+public class CandidateQualificationService implements ICandidateQualificationService {
+
+    @Autowired
+    private CandidateQualificationRepository candidateQualificationRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Override
+    public Response updateCandidateQualificationInfo(CandidateQualificationDTO candidateQualificationDTO) {
+        candidateQualificationDTO.setIsAggregatePercentageVerified("Yes");
+        candidateQualificationDTO.setIsDegreeNameVerified("Yes");
+        candidateQualificationDTO.setIsEmployeeDisciplineVerified("Yes");
+        candidateQualificationDTO.setIsFinalYearPercentageVerified("Yes");
+        candidateQualificationDTO.setIsOtherTrainingVerified("Yes");
+        candidateQualificationDTO.setIsTrainingInstituteVerified("Yes");
+        candidateQualificationDTO.setIsPassingYearVerified("Yes");
+        candidateQualificationDTO.setIsTrainingDurationMonthVerified("Yes");
+        candidateQualificationDTO.setCreatorStamp(LocalDateTime.now());
+        candidateQualificationDTO.setCreatorUser("Admin");
+        CandidateQualification candidateQualification = modelMapper.map(candidateQualificationDTO,CandidateQualification.class);
+        candidateQualificationRepository.save(candidateQualification);
+        return new Response(200, "Candidate Qualification Details Updated Successfully");
+    }
 }
--- src/main/java/com/bridgelabz/lmsapi/service/ICandidateQualificationService.java
+++ src/main/java/com/bridgelabz/lmsapi/service/ICandidateQualificationService.java
@@ -1,4 +1,8 @@
 package com.bridgelabz.lmsapi.service;
 
+import com.bridgelabz.lmsapi.dto.CandidateQualificationDTO;
+import com.bridgelabz.lmsapi.dto.Response;
+
 public interface ICandidateQualificationService {
+    Response updateCandidateQualificationInfo(CandidateQualificationDTO candidateQualificationDTO);
 }
