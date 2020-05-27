9400dda869f7c61f6a028e5cbed8a1c91c331d71
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-05-27 23:13:20 +0530

    REFACTORED : used JPA query methods

--- src/main/java/com/bridgelabz/lmsapi/repository/CandidateBankDetailsRepository.java
+++ src/main/java/com/bridgelabz/lmsapi/repository/CandidateBankDetailsRepository.java
@@ -4,6 +4,7 @@ import com.bridgelabz.lmsapi.model.CandidateBankDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface CandidateBankDetailsRepository extends JpaRepository<CandidateBankDetails, Long> {
 
--- src/main/java/com/bridgelabz/lmsapi/repository/CandidateQualificationRepository.java
+++ src/main/java/com/bridgelabz/lmsapi/repository/CandidateQualificationRepository.java
@@ -1,4 +1,10 @@
 package com.bridgelabz.lmsapi.repository;
 
-public interface CandidateQualificationRepository {
+import com.bridgelabz.lmsapi.model.CandidateQualification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CandidateQualificationRepository extends JpaRepository<CandidateQualification, Long> {
+
 }
