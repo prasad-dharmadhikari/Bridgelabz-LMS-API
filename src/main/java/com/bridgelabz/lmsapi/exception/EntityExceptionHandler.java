63e6ea0ff593a3062281524083ff218fb56a0e52
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-05-31 20:11:53 +0530

    ADDED : Spring boot global exceptions

--- src/main/java/com/bridgelabz/lmsapi/exception/EntityExceptionHandler.java
+++ src/main/java/com/bridgelabz/lmsapi/exception/EntityExceptionHandler.java
@@ -1,4 +1,40 @@
 package com.bridgelabz.lmsapi.exception;
 
-public class EntityExceptionHandler {
+import com.bridgelabz.lmsapi.response.ErrorMessageDTO;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+public class EntityExceptionHandler extends ResponseEntityExceptionHandler {
+    //Custom exception in global exception handler
+    @ExceptionHandler(LmsApiApplicationException.class)
+    public final ResponseEntity<ErrorMessageDTO> handleAnyException(Exception ex, WebRequest request) {
+        ErrorMessageDTO errorObj = new ErrorMessageDTO(new Date(), ex.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(errorObj, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    //MethodArgumentNotValidException
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ErrorMessageDTO errorObj = new ErrorMessageDTO(new Date(), "From method argument not valid exception",
+                request.getDescription(false));
+        return new ResponseEntity<>(errorObj, new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+
+    //HttpRequestMethodNotSupportedException
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ErrorMessageDTO errorObj = new ErrorMessageDTO(new Date(), "FromHttpRequestMethodNotSupportedException-Method not allowed",
+                request.getDescription(false));
+        return new ResponseEntity<>(errorObj, new HttpHeaders(), HttpStatus.METHOD_NOT_ALLOWED);
+    }
+
 }
--- src/main/java/com/bridgelabz/lmsapi/exception/LmsApiApplicationException.java
+++ src/main/java/com/bridgelabz/lmsapi/exception/LmsApiApplicationException.java
@@ -1,4 +1,20 @@
 package com.bridgelabz.lmsapi.exception;
 
-public class LmsApiApplicationException {
+public class LmsApiApplicationException extends Exception {
+
+    private static final long serialVersionUID = 5776681206288518465L;
+    private String message;
+
+    public enum exceptionType {
+        USER_NOT_FOUND, INVALID_EMAIL_ID, INVALID_PASSWORD,
+        INVALID_TOKEN, DATA_NOT_FOUND, INVALID_ID;
+    }
+
+    private exceptionType type;
+
+    public LmsApiApplicationException(exceptionType type, String message) {
+        super(message);
+        this.message = message;
+        this.type = type;
+    }
 }
