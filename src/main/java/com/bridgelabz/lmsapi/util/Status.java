e0ef538131587de3b4764c165c6825bd4d839cda
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-05-31 20:18:21 +0530

    ADDED : Some fixed values as enums

--- src/main/java/com/bridgelabz/lmsapi/util/Status.java
+++ src/main/java/com/bridgelabz/lmsapi/util/Status.java
@@ -1,4 +1,52 @@
 package com.bridgelabz.lmsapi.util;
 
 public enum Status {
+    OK {
+        @Override
+        public String toString() {
+            return "OK";
+        }
+    },
+    ACCEPTED {
+        @Override
+        public String toString() {
+            return "ACCEPTED";
+        }
+    },
+    REJECTED {
+        @Override
+        public String toString() {
+            return "REJECTED";
+        }
+    },
+    PENDING {
+        @Override
+        public String toString() {
+            return "PENDING";
+        }
+    },
+    YES {
+        @Override
+        public String toString() {
+            return "YES";
+        }
+    },
+    NO {
+        @Override
+        public String toString() {
+            return "NO";
+        }
+    },
+    VERIFIED {
+        @Override
+        public String toString() {
+            return "VERIFIED";
+        }
+    },
+    RECEIVED {
+        @Override
+        public String toString() {
+            return "RECEIVED";
+        }
+    }
 }
