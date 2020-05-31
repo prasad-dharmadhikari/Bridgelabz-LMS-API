c814a3536008381ce03c676753f3dece7b8f4c43
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-05-31 20:09:31 +0530

    REFACTORED : Minor modifications

--- src/main/java/com/bridgelabz/lmsapi/controller/UserController.java
+++ src/main/java/com/bridgelabz/lmsapi/controller/UserController.java
@@ -1,14 +1,15 @@
 package com.bridgelabz.lmsapi.controller;
 
-import com.bridgelabz.lmsapi.dto.Response;
+import com.bridgelabz.lmsapi.response.Response;
 import com.bridgelabz.lmsapi.dto.UserDTO;
+import com.bridgelabz.lmsapi.exception.LmsApiApplicationException;
 import com.bridgelabz.lmsapi.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.bridgelabz.lmsapi.dto.LoginDTO;
-import com.bridgelabz.lmsapi.dto.JwtResponse;
+import com.bridgelabz.lmsapi.response.JwtResponse;
 
 import javax.mail.MessagingException;
 
@@ -31,12 +32,12 @@ public class UserController {
     }
 
     @PostMapping("/forgot-password")
-    public ResponseEntity<Response> forgotPassword(@RequestParam String email) throws MessagingException {
+    public ResponseEntity<Response> forgotPassword(@RequestParam String email) throws MessagingException, LmsApiApplicationException {
         return ResponseEntity.ok(userService.sendMail(email));
     }
 
     @PostMapping("/reset-password")
-    public ResponseEntity<Response> resetPassword(@RequestParam String token, @RequestParam("password") String password) {
+    public ResponseEntity<Response> resetPassword(@RequestParam String token, @RequestParam("password") String password) throws LmsApiApplicationException {
         return ResponseEntity.ok(userService.resetPassword(token, password));
     }
 }
