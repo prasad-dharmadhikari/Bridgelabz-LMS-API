4db856ca069b8f2132385696214e0a51eb9f5207
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-06-07 18:33:41 +0530

    ADDED : Configuration for rabbit mq

--- src/main/java/com/bridgelabz/lmsapi/config/RabbitMqConfig.java
+++ src/main/java/com/bridgelabz/lmsapi/config/RabbitMqConfig.java
@@ -1,4 +1,52 @@
 package com.bridgelabz.lmsapi.config;
 
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class RabbitMqConfig {
+
+    @Value("${spring.rabbitmq.template.exchange}")
+    private String exchangeName;
+
+    @Value("${spring.rabbitmq.template.default-receive-queue}")
+    private String queueName;
+
+    @Value("${spring.rabbitmq.template.routing-key}")
+    private String routingKey;
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName);
+    }
+
+    @Bean
+    DirectExchange exchange() {
+        return new DirectExchange(exchangeName);
+    }
+
+    @Bean
+    Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(routingKey);
+    }
+
+//    @Bean
+//    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+//                                             MessageListenerAdapter listenerAdapter) {
+//        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+//        container.setConnectionFactory(connectionFactory);
+//        container.setQueueNames(routingKey);
+//        container.setMessageListener(listenerAdapter);
+//        return container;
+//    }
+//
+//    @Bean
+//    MessageListenerAdapter myQueueListener(MessageListener listener) {
+//        return new MessageListenerAdapter(listener, "onMessage");
+//    }
 }
