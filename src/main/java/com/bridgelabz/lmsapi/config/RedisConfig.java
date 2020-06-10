f0286ee0607a0293161c41197609cd2b315ef36e
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-06-10 21:47:33 +0530

    ADDED : Configuration for redis

--- src/main/java/com/bridgelabz/lmsapi/config/RedisConfig.java
+++ src/main/java/com/bridgelabz/lmsapi/config/RedisConfig.java
@@ -1,4 +1,43 @@
 package com.bridgelabz.lmsapi.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * Configuration for Redis database
+ */
+@Configuration
+@EnableRedisRepositories
 public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String REDIS_HOSTNAME;
+
+    @Value("${spring.redis.port}")
+    private int REDIS_PORT;
+
+    @Bean
+    public JedisConnectionFactory redisConnectionFactory() {
+        JedisConnectionFactory redisConnectionFactory = new JedisConnectionFactory();
+        redisConnectionFactory.setHostName(REDIS_HOSTNAME);
+        redisConnectionFactory.setPort(REDIS_PORT);
+        redisConnectionFactory.setUsePool(true);
+        return redisConnectionFactory;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+        template.setConnectionFactory(redisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+        return template;
+    }
 }
