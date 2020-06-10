b8e31354a5cf725aa41344557f58fdb7e94f323b
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-06-10 21:49:22 +0530

    ADDED : Utility for redis to add and delete records in redis database

--- src/main/java/com/bridgelabz/lmsapi/util/RedisUtil.java
+++ src/main/java/com/bridgelabz/lmsapi/util/RedisUtil.java
@@ -1,4 +1,36 @@
 package com.bridgelabz.lmsapi.util;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
 
-public class RedisUtil {
+import java.io.Serializable;
+
+/**
+ * Utility to handle redis related stuff
+ */
+@Component
+public class RedisUtil implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    /**
+     * @param RedisKey
+     * @param email
+     * @param authenticationToken
+     */
+    public void save(String RedisKey, String email, String authenticationToken) {
+        redisTemplate.opsForHash().put(RedisKey, email, authenticationToken);
+    }
+
+    /**
+     * @param redisIndexKey
+     * @param username
+     * @return value in redis database by key and hash key
+     */
+    public Object get(String redisIndexKey, String username) {
+        return redisTemplate.opsForHash().get(redisIndexKey, username);
+    }
 }
